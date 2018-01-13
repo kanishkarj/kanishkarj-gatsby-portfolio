0adb1c6261e625442c66249baa260f1ef3f515ac
kanishkarj <kanishkarj@hotmail.com>
2018-01-13 23:13:11 +0530

    Setup complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,10 @@ import Link from 'gatsby-link'
 
 import AboutComponent from '../components/about'
 import BlogComponent from '../components/blog'
+import ProjectComponent from '../components/project'
 
 const IndexPage = ({data}) => {
-
+console.log(data)
 return(
   <div>
     <div className="header-image-container"/>
@@ -18,6 +19,11 @@ return(
       <div className="container">
         <BlogComponent data={data.allMarkdownRemark.edges} />
       </div>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <ProjectComponent data={data.work.edges} />
+        </div>
+      </div>
     </div>
   </div>
 )
@@ -25,6 +31,25 @@ return(
 
 export const pageQuery = graphql `
   query IndexBlogQuery {
+
+      work : allMarkdownRemark(
+        limit: 3
+        filter: { frontmatter: {layout : {eq : "work"}}}
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+              title
+              subtitle
+              date
+              layout
+              categories
+              headerImg
+            }
+          }
+        }
+      }
     allMarkdownRemark(
       limit: 3
       filter: { frontmatter: {layout : {eq : "post"}}}
---
+++ src/pages/markdown/work/2015-05-01-hello-world0/index.md
@@ -0,0 +1,15 @@
+---
+title: "Programming club website"
+date: "28 May, 2015"
+layout: "work"
+subtitle: "responsive website"
+path: "/work/pcldubiiti"
+categories:
+  - Vue js
+  - Responsive
+headerImg: "https://images.pexels.com/photos/787047/pexels-photo-787047.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"
+---
+
+Johannes Gensfleisch zur Laden zum Gutenberg (c. `1398` – `1468`) was a German blacksmith, goldsmith, printer, and publisher who introduced printing to Europe.
+
+His invention of mechanical movable type printing started the Printing Revolution and is widely regarded as the most important event of the modern period. It played a key role in the development of the Renaissance, Reformation, the Age of Enlightenment, and the Scientific revolution and laid the material basis for the modern knowledge-based economy and the spread of learning to the masses.
